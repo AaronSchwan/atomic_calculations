709e1e653083539d1c87f37c4267465178948359
Aaron Schwan <43838067+AaronSchwan@users.noreply.github.com>
2020-07-06 14:38:22 -0600

    adding files

---
+++ basicFunctions.py
@@ -0,0 +1,121 @@
+#Imported by default
+import sys
+import os
+import gc
+import pickle
+import platform
+import warnings
+import pkg_resources
+import subprocess
+import ntpath
+
+
+#non-default imports
+import pandas as pd
+def pull_folder_contents(folder_path:str,file_type:str,naming_conv:str = None):
+    """
+    This function finds all files within in a folder following a certain filetype
+    as well as a naming convention. this is done by a simple look for file_type
+    in string
+
+    #####################################################################
+    pull_folder_contents(folder_path,file_type,naming_conv)
+
+    takes in:
+    folder_path = path to folder you want contents of(Unicode Str)
+    file_type = Type of file you want to select
+    nameing_conv = convention that must be present in name
+
+    file_type can be selected as numeric which will test if the extension is a
+    integer instead of a destinct file type
+
+    returns:
+    list of all file names matching description
+    """
+    temp_contents = os.listdir(folder_path)
+
+    files = []
+    #for dump files
+    if naming_conv != None:
+        if file_type == "numeric":
+            for tc in temp_contents:
+                if naming_conv in tc:
+                    file = tc.split(".")
+                    ext = file[len(file)-1]
+                    try:
+                        int(ext)
+                        files.append(tc)
+                    except:
+                        pass;
+        else:
+            for tc in temp_contents:
+                if file_type in tc and naming_conv in tc:
+                    files.append(tc)
+    else:
+        if file_type == "numeric":
+            for tc in temp_contents:
+                file = tc.split(".")
+                ext = file[len(file)-1]
+                try:
+                    int(ext)
+                    files.append(tc)
+                except:
+                    pass;
+        else:
+            for tc in temp_contents:
+                if file_type in tc:
+                    files.append(tc)
+    return files
+
+
+def removing_data(data,removed_values:dict):
+    """
+    This function takes in a pandas datframe and removes rows based on column values
+    #####################################################################
+    removing_data(data,removed_values:dict)
+
+    takes in:
+    removed_values = values to be removed (Dictionary)
+    {"Column1":[Value1a,Value1b],"Column2":[Value2a,Value2b]}
+
+    data = pandas dataframe
+
+    returns:
+    pandas dataframe minus the requesit values
+    """
+    columns = removed_values.keys()
+
+    for col in columns:
+        values = removed_values[col]
+        for ind,val in enumerate(values):
+            data = data[eval(f"data.{col}") != values[ind]]
+    return data
+
+def filtering_data(data,filtered_values:dict):
+    """
+    This function takes in a pandas datframe and removes rows based on column values
+    if they are not in the range of high and low this includes the highs and lows
+
+    #####################################################################
+
+    filtering_data(data,filtered_values)
+
+    takes in:
+    data = pandas dataframe
+    filtered_values = dictionary to filter (dict)
+    {"Column1":[high1,low1],"Column2":[high2,low2]}
+
+    returns:
+    pandas dataframe filtered
+
+    """
+    columns = filtered_values.keys()
+
+    for col in columns:
+        values = filtered_values[col]
+        high = values[0]
+        low = values[1]
+
+
+        data = data[(data[col] <= high) & (data[col] >= low)]
+    return data
---
+++ dimensionalReductionPCA.py
@@ -0,0 +1,146 @@
+#Imported by default
+import sys
+import os
+import gc
+import pickle
+import platform
+import warnings
+import pkg_resources
+import subprocess
+import ntpath
+
+#importing non-default programs
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+from sklearn import preprocessing
+import pandas as pd
+from sklearn.pipeline import Pipeline
+
+#custom imports
+import basicFunctions as bf
+import LammpsOvitoReader as lor
+
+
+class TransformData():
+    """
+    this class will transform data with a pca fit
+
+    #####################################################################
+    transform valid calls:
+    t = TransformData(file_path,pca_class,normalize)
+
+    t.transform #returns the transform of the class
+    t.file_name #returns the name of the file that was transformed
+    t.pca_serial #returns the serial number of the PCA used to transform
+    t.normalize #returns if the transform was normalized before
+    """
+
+    def __init__(self,file_path,pca_class,normalize_in):
+        self.file_name = ntpath.basename(file_path)
+        self.pca_serial = pca_class.serial
+        self.normalize = normalize_in
+
+        #Geting data
+        data = lor.dumpFile(file_path).atoms
+        data = data[pca_class.pca_columns]
+        print(data)
+        pca = pca_class.pca
+
+        #transforming data
+        self.transform = self.transform_data(data,pca,normalize_in)
+        print(self.transform)
+    def transform_data(self,data,pca,normalize):
+        if normalize == True:
+            data = pd.DataFrame(preprocessing.scale(data),columns = data.columns)#normalize data
+        pca_data = pca.transform(data)
+
+        return pca_data
+
+
+class FittingPCA():
+    """
+    This class will fit a pca to the given data and files
+
+    #####################################################################
+    fitting pca valid calls:
+
+    p = FittingPCA(pca_components,normalize,pca_columns,files,removed_values,filtered_values)
+
+    p.pca #returns the trained pca
+    p.file_names #returns names of files used to fit pca
+    p.normalize #returns if the pca used normalized data
+    p.pca_components #returns the components the pca was trained for
+    p.pca_columns #returns list of columns used to fit pca on
+    p.filtered_values #returns list of filtered data vals
+    p.removed_values #returns list of removed data vals
+    p.serial #returns serial number of the pca data
+
+    """
+
+    def __init__(self,pca_components_in:int,normalize_in:bool,pca_columns_in:list,files_in:list,removed_values_in:dict,filtered_values_in:dict,serial_in:str=None):
+
+
+        self.pca_columns = sorted(pca_columns_in)
+
+        #sorting names
+        file_names = []
+        for f in files_in:
+            file_names.append(ntpath.basename(f))
+
+
+        #importing data
+        data = self.import_data(files_in,self.pca_columns,removed_values_in,filtered_values_in)
+        print(data)
+        #saving conditions
+        self.file_names = sorted(file_names)
+        self.normalize = normalize_in
+        self.pca_components = pca_components_in
+        self.filtered_values = filtered_values_in
+        self.removed_values = removed_values_in
+        self.serial = serial_in
+        #calculating pca
+        self.pca =  self.computing_PCA(pca_components_in,data,normalize_in)
+
+        del data
+
+    def computing_PCA(self,pca_components,data,normalize):
+        fitted_pca = PCA(n_components=pca_components)#making pca structure
+
+        if normalize == True:
+            #if normalizeing needed
+            data = pd.DataFrame(preprocessing.scale(data),columns = data.columns)
+
+        fitted_pca.fit(data)#Fitting PCA
+
+        return fitted_pca
+
+
+    def import_data(self,files,pca_columns,removed_values,filtered_values):
+
+        for ind,file in enumerate(files):
+            gc.collect()#clear what it can
+            print(f"Retriving:{file}   File:{ind+1}/{len(files)}")#System out message
+
+            if ind == 0:
+                #Setting base dataframe
+                data = lor.dumpFile(file_path).atoms
+            else:
+                #appending to base dataframe
+                data_grab = lor.dumpFile(file_path).atoms
+                data.append(data_grab)
+
+        #cleaning dataframe
+        if filtered_values != {}:
+            print("Filtering Specified Data")
+            data = filtering_data(data,filtered_values)
+
+        if removed_values != {}:
+            print("Removing Specified Data")
+            data = removing_data(data,removed_values)
+
+        print("Dropping Non-PCA Columns")
+        data = data[pca_columns]
+
+        return data
